7b3fc9cf1cf1657a7bf9262e6c01d19c2797702f
Dev Ambience <125862598+Devambience@users.noreply.github.com>
2025-03-17 18:52:50 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -2,41 +2,43 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
-contextBridge.exposeInMainWorld(
-  'api', {
-    // File system operations
-    readFile: (filePath) => ipcRenderer.invoke('read-file', filePath),
-    writeFile: (filePath, content) => ipcRenderer.invoke('write-file', filePath, content),
-    readDirectory: (dirPath) => ipcRenderer.invoke('read-directory', dirPath),
-    createFile: (filePath) => ipcRenderer.invoke('create-file', filePath),
-    createDirectory: (dirPath) => ipcRenderer.invoke('create-directory', dirPath),
-    renamePath: (oldPath, newPath) => ipcRenderer.invoke('rename-path', oldPath, newPath),
-    deletePath: (pathToDelete) => ipcRenderer.invoke('delete-path', pathToDelete),
-    
-    // Dialog operations
-    showSaveDialog: (defaultPath) => ipcRenderer.invoke('show-save-dialog', defaultPath),
-    
-    // Settings operations
-    getSettings: () => ipcRenderer.invoke('get-settings'),
-    saveSettings: (settings) => ipcRenderer.invoke('save-settings', settings),
-    
-    // Menu event listeners
-    onMenuNewFile: (callback) => ipcRenderer.on('menu-new-file', callback),
-    onMenuOpenFile: (callback) => ipcRenderer.on('menu-open-file', callback),
-    onMenuOpenFolder: (callback) => ipcRenderer.on('menu-open-folder', callback),
-    onMenuSave: (callback) => ipcRenderer.on('menu-save', callback),
-    onMenuSaveAs: (callback) => ipcRenderer.on('menu-save-as', callback),
-    onMenuFind: (callback) => ipcRenderer.on('menu-find', callback),
-    onMenuReplace: (callback) => ipcRenderer.on('menu-replace', callback),
-    onMenuToggleSidebar: (callback) => ipcRenderer.on('menu-toggle-sidebar', callback),
-    onMenuToggleTerminal: (callback) => ipcRenderer.on('menu-toggle-terminal', callback),
-    onMenuNewTerminal: (callback) => ipcRenderer.on('menu-new-terminal', callback),
-    onMenuClearTerminal: (callback) => ipcRenderer.on('menu-clear-terminal', callback),
-    onMenuOpenSettings: (callback) => ipcRenderer.on('menu-open-settings', callback),
-    
-    // Remove event listeners
-    removeAllListeners: (channel) => {
-      ipcRenderer.removeAllListeners(channel);
-    }
-  }
-);
+contextBridge.exposeInMainWorld('electronAPI', {
+  // File operations
+  openFile: () => ipcRenderer.invoke('open-file'),
+  saveFile: (data) => ipcRenderer.invoke('save-file', data),
+  readFile: (filePath) => ipcRenderer.invoke('read-file', filePath),
+  saveFileAs: (filePath) => ipcRenderer.invoke('save-file-as', filePath),
+  
+  // Directory operations
+  readDirectory: (dirPath) => ipcRenderer.invoke('read-directory', dirPath),
+  createFile: (data) => ipcRenderer.invoke('create-file', data),
+  createDirectory: (data) => ipcRenderer.invoke('create-directory', data),
+  renameItem: (data) => ipcRenderer.invoke('rename-item', data),
+  deleteItem: (itemPath) => ipcRenderer.invoke('delete-item', itemPath),
+  
+  // Settings
+  getSettings: () => ipcRenderer.invoke('get-settings'),
+  saveSettings: (settings) => ipcRenderer.invoke('save-settings', settings),
+  
+  // Listen to menu events
+  onMenuNewFile: (callback) => ipcRenderer.on('menu-new-file', callback),
+  onMenuSave: (callback) => ipcRenderer.on('menu-save', callback),
+  onMenuFind: (callback) => ipcRenderer.on('menu-find', callback),
+  onMenuReplace: (callback) => ipcRenderer.on('menu-replace', callback),
+  onFileOpened: (callback) => ipcRenderer.on('file-opened', callback),
+  onFolderOpened: (callback) => ipcRenderer.on('folder-opened', callback),
+  onSaveFileAs: (callback) => ipcRenderer.on('save-file-as', callback),
+  onToggleSidebar: (callback) => ipcRenderer.on('toggle-sidebar', callback),
+  onToggleTerminal: (callback) => ipcRenderer.on('toggle-terminal', callback),
+  onToggleLineNumbers: (callback) => ipcRenderer.on('toggle-line-numbers', callback),
+  onToggleWordWrap: (callback) => ipcRenderer.on('toggle-word-wrap', callback),
+  onNewTerminal: (callback) => ipcRenderer.on('new-terminal', callback),
+  onClearTerminal: (callback) => ipcRenderer.on('clear-terminal', callback),
+  onChangeTheme: (callback) => ipcRenderer.on('change-theme', callback),
+  onOpenSettings: (callback) => ipcRenderer.on('open-settings', callback),
+  onFileOpenedFromSystem: (callback) => ipcRenderer.on('file-opened-from-system', callback),
+  onDeepLink: (callback) => ipcRenderer.on('deep-link', callback),
+  
+  // Remove event listeners
+  removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel)
+});
